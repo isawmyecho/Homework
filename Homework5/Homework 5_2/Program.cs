f5dc464b5b674d01d4f9a20dd646bb1c0fdf7d73
Karina <breathless.suffering@gmail.com>
2023-06-30 13:45:26 +0400

    Homework 5_2

---
+++ Homework5/Homework 5_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+Console.WriteLine("Enter the size of the array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("This is an array: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int index = 1; index < numbers.Length; index+=2)
+    sum = sum + numbers[index];
+
+    Console.WriteLine($"There are {numbers.Length} numbers, sum of elements in odd positions = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
--- Homework5/Homework 5_2/obj/Debug/net8.0/Homework 5_2.AssemblyInfo.cs
+++ Homework5/Homework 5_2/obj/Debug/net8.0/Homework 5_2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Homework 5_1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Homework 5_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Homework 5_1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Homework 5_1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4b1e1d8aa0b3848277d0b60093f32e094652b3c")]
+[assembly: System.Reflection.AssemblyProductAttribute("Homework 5_2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Homework 5_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
