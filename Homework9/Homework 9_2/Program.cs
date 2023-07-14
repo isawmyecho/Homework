bcbf073409e2f28075f2c403b835e1ca8155c856
Karina <breathless.suffering@gmail.com>
2023-07-14 13:32:00 +0400

    Homework 9_3

---
+++ Homework9/Homework 9_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+int m = InputInt("Input M:");
+int n = InputInt("Input N:");
+Console.WriteLine($"Sum of elements from {m} to {n} = {NaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int NaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + NaturalSum(m, n - 1);
+}
