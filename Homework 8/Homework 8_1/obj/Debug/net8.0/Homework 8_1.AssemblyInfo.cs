a90c535079d90d0283d0bdb62022f714fd6cfa44
Karina <breathless.suffering@gmail.com>
2023-07-18 14:04:25 +0400

    Homework 8_1

--- Homework 8/Homework 8_1/obj/Debug/net8.0/Homework 8_1.AssemblyInfo.cs
+++ Homework 8/Homework 8_1/obj/Debug/net8.0/Homework 8_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework 8_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e5107103da13adf1e2bc305901bc3e6f3ff2ef7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework 8_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework 8_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
